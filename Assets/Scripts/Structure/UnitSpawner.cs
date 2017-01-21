5f980fe69fcb233610a3d0e7886a84ee29b21b62
germanosk <germanobioinfo@gmail.com>
2017-01-21 19:53:56 -0400

    Creating Squad and Spawner

---
+++ Assets/Scripts/Structure/UnitSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeardTwins.TO {
+    public class UnitSpawner : MonoBehaviour {
+        public Squad squadPrefab;
+
+        public float dirSpawnAngle = 0.0f;
+
+        public Vector3 SpawnDir
+        {
+            get { return Quaternion.Euler(0, 0, dirSpawnAngle) * transform.right; }
+        }
+
+        public Vector3 SpawnPos
+        {
+            get { return transform.position + SpawnDir; }
+        }
+
+        void Start()
+        {
+            SpawnSquad();
+        }
+
+        private void SpawnSquad()
+        {
+            Squad temp = Instantiate<Squad>(squadPrefab);
+            temp.transform.position = SpawnPos;
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Vector3 dirSpawn = SpawnDir;
+            Gizmos.DrawLine(transform.position, transform.position+dirSpawn);
+        }
+    }
+}
---
+++ Assets/Scripts/Unit/Squad.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BeardTwins.TO
+{
+    public class Squad : MonoBehaviour
+    {
+        
+    }
+}
