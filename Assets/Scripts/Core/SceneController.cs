f9397a843fd3681dd5819f0d52d41a6cf1ec4e47
Tadman Reis <tadman.freis@hotmail.com>
2017-03-31 02:14:37 -0300

    Implemented Fast Foward feature.

--- Assets/Scripts/Core/SceneController.cs
+++ Assets/Scripts/Core/SceneController.cs
@@ -6,6 +6,15 @@ namespace BeardTwins.TO
 {
     public class SceneController : MonoBehaviour
     {
+        public int timeScale = 1;
+        public float defaultFixedDeltaTime;
+
+
+        public void Awake()
+        {
+            defaultFixedDeltaTime = Time.fixedDeltaTime;
+        }
+
         public void LoadScene(string name) {
             SceneManager.LoadScene(name);
         }
@@ -17,6 +26,19 @@ namespace BeardTwins.TO
         public void Quit()
         {
             Application.Quit();
+        }
+
+        //Method used by the FowardButton
+        public void ChangeTimeScale() {
+            if (timeScale == 1) {
+                ++this.timeScale;
+                Time.fixedDeltaTime = Time.fixedDeltaTime * this.timeScale;
+            }
+            else {
+                timeScale = 1;
+                Time.fixedDeltaTime = this.defaultFixedDeltaTime;
+            }
+            Time.timeScale = timeScale;
         }
     }
 }
